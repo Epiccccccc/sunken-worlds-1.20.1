3c68dcc5dd1524af23b9e8dc5be2e7500ab1b43f
Epicc <Epiccccc07@outlook.com>
2024-05-19 20:56:51 +0100

    Gup

---
+++ src/main/java/net/epicsaurus/sunkenworlds/SunkenWorldsClient.java
@@ -0,0 +1,10 @@
+package net.epicsaurus.sunkenworlds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SunkenWorldsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

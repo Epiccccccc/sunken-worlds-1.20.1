e657bb6f2d9086fb5420c73c28feb6588421fac7
Epicc <Epiccccc07@outlook.com>
2024-05-20 08:17:08 +0100

    Gup

--- src/main/java/net/epicsaurus/sunkenworlds/item/ModItemGroups.java
+++ src/main/java/net/epicsaurus/sunkenworlds/item/ModItemGroups.java
@@ -1,6 +1,7 @@
 package net.epicsaurus.sunkenworlds.item;
 
 import net.epicsaurus.sunkenworlds.SunkenWorlds;
+import net.epicsaurus.sunkenworlds.block.ModBlocks;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -16,6 +17,9 @@ public class ModItemGroups {
                     .icon(() -> new ItemStack(ModItems.SHARKTOOTH)).entries((displayContext, entries) -> {
                     entries.add(ModItems.BLUBBER);
                     entries.add(ModItems.SHARKTOOTH);
+
+                    entries.add(ModBlocks.CAT_BLOCK);
+
                     }).build());
 
     public static void registeritemgroups() {
